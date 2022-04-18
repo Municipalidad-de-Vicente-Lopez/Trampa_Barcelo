65916b305dafef8aed86d73b81e148cd61f45589
Mario Tomás Garcia Fernandez Schieder <74739978+MariioTomas@users.noreply.github.com>
2022-04-18 12:58:43 -0300

    Update Barcelo_Script.py
    Actualización

--- Barcelo_Script.py
+++ Barcelo_Script.py
@@ -1,15 +1,43 @@
+import cv2
 import time
-from picamera import Picamera
-from time import sleep
+import os
 
-i=1
-tiempoCaptura = 21600
-camara = Picamera()
+cam = cv2.VideoCapture(0)
+path = "/home/pi/mnt/googledrive"
+i=7
+tiempo= 43200 #43200 segundos = 12 horas.
 
-while true:
-  camara.start_preview()
-  camara.capture("/home/pi/Pictures/captura"+str(i)+".jpg")
-  camara.stop_preview()
-  i=i+1
-  time.sleep(tiempocaptura)
-                 
+#-Resolucion de la imagen-
+cam.set(3, 2560)
+cam.set(4, 1600)
+
+time.sleep(10)
+image = cam.read()[1]
+time.sleep(10)
+cv2.imwrite(os.path.join(path,"captura"+"1.png"),image)
+time.sleep(10)
+image = cam.read()[1]
+time.sleep(10)
+cv2.imwrite(os.path.join(path,"captura"+"2.png"),image)
+time.sleep(10)
+image = cam.read()[1]
+time.sleep(10)
+cv2.imwrite(os.path.join(path,"captura"+"3.png"),image)
+time.sleep(10)
+image = cam.read()[1]
+time.sleep(10)
+cv2.imwrite(os.path.join(path,"captura"+"4.png"),image)
+time.sleep(10)
+image = cam.read()[1]
+time.sleep(10)
+cv2.imwrite(os.path.join(path,"captura"+"5.png"),image)
+time.sleep(10)
+image = cam.read()[1]
+time.sleep(10)
+cv2.imwrite(os.path.join(path,"captura"+"6.png"),image)
+
+while True:
+    image = cam.read()[1]
+    cv2.imwrite(os.path.join(path,"captura"+str(i)+".png"),image)
+    time.sleep(tiempo)
+    i = i+1
